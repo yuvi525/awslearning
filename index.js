96bb830be330d9dd60f64ef7e44f128c3cbc547d
EC2 Default User <ec2-user@ip-10-0-1-115.ec2.internal>
2026-07-03 13:55:27 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello Yuvraj! Welcome to your first Node.js app on AWS 🚀");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
